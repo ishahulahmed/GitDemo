a11e9baa1966de6036b6c145bb7357c27e59bded
shahu <shahulahmed1717@gmail.com>
2023-05-29 12:52:25 +0530

    demo merge is being added

---
+++ src/SeleniumCourse/DemoMerge.java
@@ -0,0 +1,18 @@
+package SeleniumCourse;
+
+public class DemoMerge {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 5; i++) {
+			System.out.println(i);
+		}
+
+		System.out.println("redmi");
+		System.out.println("xiaomi");
+		System.out.println("iphone");
+		System.out.println("mi");
+
+	}
+
+}
